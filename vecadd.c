4aa1b6590e984a0fb4fe184488d901115aa68b11
Milind712000 <email2milindtopno.test@gmail.com>
2020-10-14 15:51:09 +0530

    implement vector addition

--- vecadd.c
+++ vecadd.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
 void vectorAdd(float v1[4], float v2[4]){ // prints sum of 2 vectors
-    printf("addition of 2 vectors\n");
+    printf("Addition of 2 vectors : (%.3f,%.3f,%.3f,%.3f)", v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2], v1[3]+v2[3]);
+    printf("\n");
+    return;
 }
