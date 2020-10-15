ddf1bf5ace076bd0c45689af0fc5bfc067943b3d
random-online-person <yash.tg00@gmail.com>
2020-10-15 19:26:32 +0530

    vecnorm function

--- vecnorm.c
+++ vecnorm.c
@@ -1,4 +1,15 @@
+// author Yash Chaudhary b18094
+
 #include<stdio.h>
+#include <math.h>
+
 void vectorNorm(float v1[4], float v2[4]){
-    printf("norm of 2 vectors\n");
+
+	float norm = 0;
+	for(int i = 0; i < 4; i++){
+		norm += pow((v2[i] - v1[i]), 2);
+	}
+	norm = sqrt(norm);
+
+    printf("norm of 2 vectors is %f \n", norm);
 }
