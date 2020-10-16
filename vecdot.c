83d1d01c53b63194d14cfc29fd80c095b446e5e9
Rahul <rs4231199@gmail.com>
2020-10-16 12:10:03 +0530

    Updated vecdot.c

--- vecdot.c
+++ vecdot.c
@@ -1,4 +1,4 @@
 #include<stdio.h>
 void vectorDot(float v1[4], float v2[4]){
-    printf("dot of 2 vectors\n");
+    printf("dot of 2 vectors : %.3f\n", v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] + v1[3]*v2[3]);
 }
